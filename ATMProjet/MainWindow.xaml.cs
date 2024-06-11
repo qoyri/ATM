42362c3efc33a9d96d4ea97b2b02a9d3e3e810ba
Benoit Mathiez <benoit.mathiez@saintmichelannecy.fr>
2024-06-11 12:32:21 +0200

    Ajoutez des fichiers projet.

---
+++ ATMProjet/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATMProjet
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartLoading();
+        }
+
+        private async void StartLoading()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                LoadingProgressBar.Value = i;
+                LoadingText.Text = $"{i}%";
+                await Task.Delay(30);
+
+                Dispatcher.Invoke(() => { }, System.Windows.Threading.DispatcherPriority.ContextIdle);
+            }
+
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
+        }
+    }
+}
