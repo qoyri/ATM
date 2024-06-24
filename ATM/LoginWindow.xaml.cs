0d1e6f22a05448fc99566ea77c81e15354d73681
Benoit Mathiez <benoit.mathiez@saintmichelannecy.fr>
2024-06-24 11:59:06 +0200

    login

---
+++ ATM/LoginWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ATMProjet
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bienvenue !!!!!!!!!!!!!!!!");
+
+        }
+    }
+}
