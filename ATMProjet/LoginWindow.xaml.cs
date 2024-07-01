9e883948c72cb83fd5513a34a7900d20250904e1
zyixcw <163128109+zyixcw@users.noreply.github.com>
2024-07-01 12:44:15 +0200

    Add files via upload

---
+++ ATMProjet/LoginWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ATMProjet
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bienvenue !");
+        }
+    }
+}
